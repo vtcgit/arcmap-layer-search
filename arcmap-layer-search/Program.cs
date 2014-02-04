6efc019bf48c5d6821147bf9394744a33c192471
Kyle Schutt <kschutt@gmail.com>
2014-02-03 22:21:21 -0500

    adding solution to project

---
+++ arcmap-layer-search/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace arcmap_layer_search
+{
+    class Program
+    {
+        [STAThread()]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
