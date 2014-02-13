ef044196fdc338fdc5f185e0bd59001719a4fae5
Kyle Schutt <kschutt@gmail.com>
2014-02-13 10:11:13 -0500

    checking for data source fixes #4

--- arcmap-layer-search/Models/LayerEntry.cs
+++ arcmap-layer-search/Models/LayerEntry.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace arcmap_layer_search.Models
 {
@@ -12,10 +13,12 @@ namespace arcmap_layer_search.Models
             this.LayerName = layerName;
             this.Location = location;
             this.MxdOrigin = mxd;
+            this.LayerExists = Directory.Exists(location) ? "Yes" : "No";
         }
 
         public string LayerName { get; private set; }
         public string Location { get; private set; }
         public string MxdOrigin { get; private set; }
+        public string LayerExists { get; private set; }
     }
 }
